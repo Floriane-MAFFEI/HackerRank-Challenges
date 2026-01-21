a292256ba9f0deaabf77483fd1c0bba0b2009c6b
Floriane-MAFFEI <shadowdevzone@gmail.com>
2026-01-21 17:57:33 +0100

    feat(sql/easy): add Select All solution

---
+++ SQL/Easy/Select-All.sql
@@ -0,0 +1,24 @@
+/**
+ * HackerRank – SQL
+ * Query: Select All
+ * Difficulty: Easy
+ *
+ * Objectif :
+ * Récupérer toutes les colonnes de toutes les lignes
+ * de la table CITY.
+ *
+ * Problème reformulé :
+ * 1) Utiliser la table CITY
+ * 2) Sélectionner toutes les colonnes
+ * 3) Aucun filtre n’est appliqué : toutes les lignes doivent apparaître
+ *
+ * Raisonnement :
+ * - SELECT * permet de récupérer toutes les colonnes rapidement
+ * - FROM CITY indique la table cible
+ * - Pas de clause WHERE nécessaire
+ * - Utile pour explorer la table avant d’appliquer des filtres
+ *
+ */
+
+SELECT *
+FROM CITY;
